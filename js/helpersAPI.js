3c4c9cc9371fc36f581759ab5f2dc60b082d169d
qwark97 <marcin.plata1@gmail.com>
2020-04-30 20:57:57 +0200

    remove unused function

--- js/helpersAPI.js
+++
@@ -1,8 +0,0 @@
-let getWeather = async (lon, lat) => {
-	let currentWeatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=b03accfd92a0ca5f70a918b8f9b725b4`
-	
-	let resp = await fetch(currentWeatherURL)
-					.then(res => res.json())
-					.catch(er => console.log(er))
-	console.log(resp)
-}
