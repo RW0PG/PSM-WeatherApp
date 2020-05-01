23f663178b03de2de7aea4d0b80456a3fb76e2f0
karol maczuga <karolmaczuga@gmail.com>
2020-05-01 13:30:23 +0200

    fixed pwa installation and favicons

---
+++ serviceWorker.js
@@ -0,0 +1,33 @@
+const staticWeatherApp = "psm-weather-app-v1"
+
+const assets = [
+  "/",
+  "/authentication.html",
+  "/details.html",
+  "/localization.html",
+  "/index.html",
+  "/css/style.css",
+  "/js/index.js",
+  "/js/firebaseAuth.js",
+  "/js/firebaseConfig.js",
+  "/js/gMapsConfig.js",
+  "/js/logout.js",
+  "/images/back-arrow.png",
+  "/images/plus-button.png"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticWeatherApp).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
