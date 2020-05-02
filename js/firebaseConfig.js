06551381a5f82c1941419888ea722e94b36d43d1
qwark97 <marcin.plata1@gmail.com>
2020-05-02 22:55:54 +0200

    rm conf

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,12 +1,12 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyCLvpxS3gUoKuEw_0E5D8LEDHvpYN_Jbh0",
-  authDomain: "http://psm-weatherapp.firebaseapp.com/",
-  databaseURL: "https://psm-weatherapp.firebaseio.com/",
-  projectId: "psm-weatherapp",
-  storageBucket: "http://psm-weatherapp.appspot.com/",
-  messagingSenderId: "58715636644",
-  appId: "1:58715636644:web:42ad8ecd6cbe0288843731"
-};
+// var firebaseConfig = {
+//   apiKey: "",
+//   authDomain: "",
+//   databaseURL: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: ""
+// };
 
 
 // Initialize Firebase
