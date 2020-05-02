59ca2d81eb0d8fcfeff35a197b369bd0646ccc5b
qwark97 <marcin.plata1@gmail.com>
2020-05-02 22:33:23 +0200

    test push config

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,16 +1,12 @@
-/*var firebaseConfig = {
-    apiKey: "api-key",
-    authDomain: "project-id.firebaseapp.com",
-    databaseURL: "https://project-id.firebaseio.com",
-    projectId: "project-id",
-    storageBucket: "project-id.appspot.com",
-    messagingSenderId: "sender-id",
-    appId: "app-id",
-    measurementId: "G-measurement-id",
-  };
-  firebase.initializeApp(firebaseConfig);
-  */
- 
+var firebaseConfig = {
+  apiKey: "AIzaSyCLvpxS3gUoKuEw_0E5D8LEDHvpYN_Jbh0",
+  authDomain: "http://psm-weatherapp.firebaseapp.com/",
+  databaseURL: "https://psm-weatherapp.firebaseio.com/",
+  projectId: "psm-weatherapp",
+  storageBucket: "http://psm-weatherapp.appspot.com/",
+  messagingSenderId: "58715636644",
+  appId: "1:58715636644:web:42ad8ecd6cbe0288843731"
+};
 
 
 // Initialize Firebase
